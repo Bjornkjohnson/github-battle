017d8d4da14634450b0c43e9f4935df786739c3f
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-06-01 13:15:47 -0400

    Add generic player form

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,13 +6,16 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
 
 var routes = (
-    <Router history={hashHistory}>
-        <Route path='/' component={Main}>
-            <IndexRoute component={Home} />
-        </Route>
-    </Router>
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={Home} />
+      <Route path='playerOne' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' component={PromptContainer} />
+    </Route>
+  </Router>
 );
 
 module.exports = routes;
