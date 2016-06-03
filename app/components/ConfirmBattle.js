7cbec10a0976017807dc19fbcc091ade0849df48
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-06-03 10:57:11 -0400

    Build Loading Component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,46 +1,43 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
-var Link = require('react-router').Link;
-var UserDetails = require('./UserDetails');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var UserDetails = require('./UserDetails');
 var MainContainer = require('./MainContainer');
-
+var Loading = require('./Loading');
 
 function ConfirmBattle (props) {
-    return props.isLoading === true
-    ? <p> LOADING! </p>
+  return props.isLoading === true
+    ? <Loading />
     : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
-            <UserDetailsWrapper header='Player One'>
-                <UserDetails info={props.playersInfo[0]}/>
-            </UserDetailsWrapper>
-            <UserDetailsWrapper header='Player Two'>
-                <UserDetails info={props.playersInfo[1]}/>
-            </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player 1'>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player 2'>
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
-            <div className='col-sm-12' style={styles.space}>
-                <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
-                    INITIATE BATTLE
-                </button>
-            </div>
-            <div className='col-sm-12' style={styles.space}>
-                <Link to ='/playerOne'>
-                    <button type='button' className='btn btn-lg btn-danger' onClick={props.onClick}>
-                        Reselect Players
-                    </button>
-                </Link>
-            </div>
-            </div>
+          <div className='col-sm-12' style={styles.space}>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
+          </div>
+          <div className='col-sm-12' style={styles.space}>
+            <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
+            </Link>
+          </div>
+        </div>
       </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
-    isLoading: PropTypes.bool.isRequired,
-    onInitiateBattle: PropTypes.func.isRequired,
-    playersInfo: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired,
 }
 
 module.exports = ConfirmBattle;
